98362011c73dcbf4499cbd89be486f6fc404f13d
Dpoche07 <dpoche07@gmail.com>
2024-11-26 12:58:27 -0500

    OK

--- punto5.py
+++ punto5.py
@@ -1,7 +1,7 @@
 total_notas = 0
 contador_notas = 0
 cantidad_notas = int(input("Notas a ingresar: "))
-while contador_notas * cantidad_notas:
+while contador_notas < cantidad_notas:
     nota = float(input(f"ingresa la nota {contador_notas + 1} (entre 0 y 5.0):"))
     if 0 <= nota <= 5.0:
         total_notas += nota
