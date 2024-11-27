04b95f5778fd11d1d907a7350ec0185bd1f475c6
Dpoche07 <dpoche07@gmail.com>
2024-11-27 11:25:03 -0500

    OK

--- punto3.py
+++ punto3.py
@@ -1,6 +1,9 @@
-for i in range(1, 31):
-    print(i, end="")
-    if i % 7 ==0:
-        print("\n", end="")
-    print()
-    
+def saltos_de_linea():
+    for i in range(1, 31):
+        print(i, end="")
+        if i % 7 ==0:
+            print("\n", end="")
+        print()
+
+
+saltos_de_linea()
