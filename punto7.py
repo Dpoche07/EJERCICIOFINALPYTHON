e07cd4900c220ba31532704095bfdb479c2eba7e
Dpoche07 <dpoche07@gmail.com>
2024-11-26 19:56:53 -0500

    OK

---
+++ punto7.py
@@ -0,0 +1,8 @@
+nombre_aprendiz = input("Escriba su nombre: ")
+asignatura = input("Escriba su asignatura: ")
+nota_final = int(float(input("Digite su nota: ")))
+
+if nota_final > 7:
+    print("Aprobado")
+else:
+    print("Reprobado")
