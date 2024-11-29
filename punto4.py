5e9e0de5d2cce172cf3c53c342fc1498f0f6ba6e
Dpoche07 <dpoche07@gmail.com>
2024-11-29 10:30:39 -0500

    ejercicio final con menu

--- punto4.py
+++ punto4.py
@@ -4,9 +4,17 @@ def calcular_inversion():
         interes_anual = float(input("ingresa el interes anual (en %): "))
         tiempo = int(input("digita el numero de años: "))
         capital_final = capital_inicial * (1 + interes_anual / 100)** tiempo
+        if interes_anual > 15:
+            nuevo_capital = capital_final + 20000
+        else:
+            nuevo_capital = capital_final 
 
-        print(f"\n despues de {tiempo} años su inversión será de ${capital_final:.2f}")
+        print(f"\n despues de {tiempo} años su inversión será de ${capital_final:,.2f}")
+        print(f"El nuevo valor es de: ${nuevo_capital:,.2f}")
     except ValueError:
         print("por favor, ingresa valores numéricos validos")
+
+    
+    
 if __name__ == "__main__":
     calcular_inversion()
