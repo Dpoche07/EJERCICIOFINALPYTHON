b2c0d92934eb8d42a6286c6aeba2b961b2585397
Dpoche07 <dpoche07@gmail.com>
2024-11-29 06:57:49 -0500

    ejercicio final con menu

---
+++ menu.py
@@ -0,0 +1,16 @@
+import os
+
+opcion = ""
+
+def importefila(opcion):
+    nombre_fila = f"punto{opcion}.py"
+    if os.path.exists(nombre_fila):
+        exec(open(nombre_fila).read())
+    else:
+        print(f"{nombre_fila} no existe.")
+
+while opcion != "salir":
+    opcion = input("Elige un punto para ejecuar (1, 2, 3, 4, 5, 6, 7, 8, 9, 10 salir): ")
+    if opcion == "salir":
+        break
+    importefila(opcion)
